b9cf84dd334416e268fbb896cbf328c8749b9847
zentochi <danuarta241@gmail.com>
2023-11-20 14:23:34 +0700

    initial commit

---
+++ app/src/main/java/com/ps420/semaphoreapps/ui/home/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.ps420.semaphoreapps.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ps420.semaphoreapps.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
