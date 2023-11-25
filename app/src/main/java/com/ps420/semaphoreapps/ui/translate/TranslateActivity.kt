d4fac86cb9e7281c0adc0827cee4d84e49da41f0
Ihsan Hadiansyach <ihadiansyach@gmail.com>
2023-11-25 19:42:55 +0700

    feat: add UI splashscreen, translate image

--- app/src/main/java/com/ps420/semaphoreapps/ui/translate/TranslateActivity.kt
+++ app/src/main/java/com/ps420/semaphoreapps/ui/translate/TranslateActivity.kt
@@ -1,13 +1,12 @@
-package com.ps420.semaphoreapps.ui.get_started
+package com.ps420.semaphoreapps.ui.translate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ps420.semaphoreapps.R
 
-class WelcomeActivity : AppCompatActivity() {
+class TranslateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        setContentView(R.layout.activity_translate)
     }
-
 }
