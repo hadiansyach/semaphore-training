2aefe6966f9ebb829994c38aacaf5cc34bcbdf5f
Ihsan Hadiansyach <ihadiansyach@gmail.com>
2023-12-03 18:35:31 +0700

    fix(ui): navigate button at home activity and adjust learn layout

--- app/src/main/java/com/ps420/semaphoreapps/ui/home/HomeActivity.kt
+++ app/src/main/java/com/ps420/semaphoreapps/ui/home/HomeActivity.kt
@@ -1,15 +1,40 @@
 package com.ps420.semaphoreapps.ui.home
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import com.ps420.semaphoreapps.R
+import androidx.appcompat.app.AppCompatActivity
+import com.ps420.semaphoreapps.databinding.ActivityHomeBinding
 import com.ps420.semaphoreapps.ui.learn.LearnActivity
+import com.ps420.semaphoreapps.ui.setting.SettingActivity
+import com.ps420.semaphoreapps.ui.translate.TranslateActivity
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+//        Button
+        binding.btnLearn.setOnClickListener {
+            val intent = Intent(this, LearnActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnPractice.setOnClickListener {
+            val intent = Intent(this, LearnActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnTranslate.setOnClickListener {
+            val intent = Intent(this, TranslateActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnSetting.setOnClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
