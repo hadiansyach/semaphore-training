60f2444d7ea38a85b7ea836404ca2caf948dd391
Ihsan Hadiansyach <ihadiansyach@gmail.com>
2023-12-14 08:32:48 +0700

    chore: set retrofit configuration to consume API

---
+++ app/src/main/java/com/ps420/semaphoreapps/data/ModelApiService.kt
@@ -0,0 +1,9 @@
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+//    @GET("detail/{id}")
+//    fun getModel(
+//        @Path("id") id: String
+//    ): Call<"ModelResponse">
+}
---
+++ app/src/main/java/com/ps420/semaphoreapps/data/retrofit/ModelApiService.kt
@@ -0,0 +1,4 @@
+package com.ps420.semaphoreapps.data.retrofit
+
+interface ModelApiService {
+}
