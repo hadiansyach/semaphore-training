f746f7e646aa5bbbce2965d78314a839690ae708
zentochi <danuarta241@gmail.com>
2023-12-18 21:47:22 +0700

    fix(dayNigh preference): fix dayNight preference wont saved
    - fix dayNight preference wont saved
    - comment out unused `SettingViewModel.kt, SettingPreferences.kt`, and `SettingViewModelFactory.kt`
    (bisa dihapus)
    - Adding Semaphoreapps.kt, and configured it to AndroidManifest.xml

--- app/src/main/java/com/ps420/semaphoreapps/view/setting/SettingPreferences.kt
+++ app/src/main/java/com/ps420/semaphoreapps/view/setting/SettingPreferences.kt
@@ -9,32 +9,32 @@ import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "Setting")
-
-class SettingPreferences private constructor(private val dataStore: DataStore<Preferences>) {
-    private val THEME_KEY = booleanPreferencesKey("theme_setting")
-    fun getThemeSetting(): Flow<Boolean> {
-        return dataStore.data.map { preferences ->
-            preferences[THEME_KEY] ?: false
-        }
-    }
-
-    suspend fun saveThemeSetting(isDarkModeActive: Boolean) {
-        dataStore.edit { preferences ->
-            preferences[THEME_KEY] = isDarkModeActive
-        }
-    }
-
-    companion object {
-        @Volatile
-        private var INSTANCE: SettingPreferences? = null
-
-        fun getInstance(dataStore: DataStore<Preferences>): SettingPreferences {
-            return INSTANCE ?: synchronized(this) {
-                val instance = SettingPreferences(dataStore)
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
-}
+//val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "Setting")
+//
+//class SettingPreferences private constructor(private val dataStore: DataStore<Preferences>) {
+//    private val THEME_KEY = booleanPreferencesKey("theme_setting")
+//    fun getThemeSetting(): Flow<Boolean> {
+//        return dataStore.data.map { preferences ->
+//            preferences[THEME_KEY] ?: false
+//        }
+//    }
+//
+//    suspend fun saveThemeSetting(isDarkModeActive: Boolean) {
+//        dataStore.edit { preferences ->
+//            preferences[THEME_KEY] = isDarkModeActive
+//        }
+//    }
+//
+//    companion object {
+//        @Volatile
+//        private var INSTANCE: SettingPreferences? = null
+//
+//        fun getInstance(dataStore: DataStore<Preferences>): SettingPreferences {
+//            return INSTANCE ?: synchronized(this) {
+//                val instance = SettingPreferences(dataStore)
+//                INSTANCE = instance
+//                instance
+//            }
+//        }
+//    }
+//}
--- app/src/main/java/com/ps420/semaphoreapps/view/setting/SettingViewModelFactory.kt
+++ app/src/main/java/com/ps420/semaphoreapps/view/setting/SettingViewModelFactory.kt
@@ -3,12 +3,12 @@ package com.ps420.semaphoreapps.view.setting
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class SettingViewModelFactory(private val preferences: SettingPreferences)
-    : ViewModelProvider.NewInstanceFactory() {
-    @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(SettingViewModel::class.java)) {
-            return SettingViewModel(preferences) as T
-        } else throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
-    }
-}
+//class SettingViewModelFactory(private val preferences: SettingPreferences)
+//    : ViewModelProvider.NewInstanceFactory() {
+//    @Suppress("UNCHECKED_CAST")
+//    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+//        if (modelClass.isAssignableFrom(SettingViewModel::class.java)) {
+//            return SettingViewModel(preferences) as T
+//        } else throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+//    }
+//}
